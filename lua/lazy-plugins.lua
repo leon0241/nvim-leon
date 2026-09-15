82f629ac75be21b0d292bd5fee95f47eb8ea8559
Leon024 <50549506+leon0241@users.noreply.github.com>
2026-09-15 11:08:42 +0100

    remove telescope

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -68,7 +68,7 @@ require('lazy').setup({
     'jalvesaq/zotcite',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
-      'nvim-telescope/telescope.nvim',
+      -- 'nvim-telescope/telescope.nvim',
     },
   },
 
@@ -172,7 +172,7 @@ require('lazy').setup({
     },
   },
 
-  -- Treesitter (RIP)
+  -- Treesitter (RIP?)
   {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main', -- Ensure you are on main, NOT master
@@ -184,25 +184,25 @@ require('lazy').setup({
     },
   },
 
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-telescope/telescope-bibtex.nvim',
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-  },
+  -- -- Fuzzy Finder (files, lsp, etc)
+  -- {
+  --   'nvim-telescope/telescope.nvim',
+  --   branch = '0.1.x',
+  --   dependencies = {
+  --     'nvim-telescope/telescope-bibtex.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  --     -- Only load if `make` is available. Make sure you have the system
+  --     -- requirements installed.
+  --     {
+  --       'nvim-telescope/telescope-fzf-native.nvim',
+  --       build = 'make',
+  --       cond = function()
+  --         return vim.fn.executable 'make' == 1
+  --       end,
+  --     },
+  --   },
+  -- },
 
   -- ╭─────────────────────────────────────────────────────────╮
   -- │                    LSP Configuration                    │
