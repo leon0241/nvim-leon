56d3d84402b09a62113cfa3a6c5184626e7d1c56
Leon024 <50549506+leon0241@users.noreply.github.com>
2026-09-22 11:31:59 +0100

    NUKE

--- init.lua
+++ init.lua
@@ -4,24 +4,4 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- todo
 
--- Install lazy package manager
-require 'lazy-install'
-
--- Plugins
-require 'lazy-plugins'
-
--- Neovim options
-require("custom.options")
-
--- Plugin setups
-require 'custom.plugin-config'
-
--- Mappings
-require 'custom.mappings'
-
--- require("custom.init")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
